06ed610279051e124fe930d06f856079feb69f58
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2022-08-22 10:55:42 -0500

    Updating motor driver src code from jetson_nano branch

--- catkin_ws/src/asinus_car/src/motor_driver.py
+++ catkin_ws/src/asinus_car/src/motor_driver.py
@@ -5,23 +5,23 @@ from donkietown_msgs.msg import MotorsSpeed, MotorsState
 from time import sleep
 
 class motor_driver_node: 
-	def __init__(self,car_id,i2c_bus=1,i2c_addr=0x08): 
+	def __init__(self,car_id): 
 		self.car_id = car_id
-		self.motors = motors(i2c_bus,i2c_addr)
+		self.motors = motors()
 		self.motors.stop()
 		rospy.on_shutdown(self.shutdown)
 		self.measures_pub = rospy.Publisher('/asinus_cars/'+str(car_id)+'/motors_raw_data',MotorsState,queue_size=1)
 		self.driver_sub = rospy.Subscriber('/asinus_cars/'+str(car_id)+'/motors_driver',MotorsSpeed, self.on_drive)
 		self.motor_st = MotorsState()
 		#self.motor_st.header.frame_id = "base_link" #Unnecessary 
-	def on_drive(self,speed_msg): 
+	def on_drive(self,speed_msg):
 		self.motors.setSpeeds([speed_msg.leftMotor,speed_msg.rightMotor])
 		self.motor_st.header.stamp = rospy.Time.now()
 		sleep(0.01) #Increase time gap between i2c transactions.
 		(sp_left,sp_right,volt) = self.motors.getMeasures()
-		self.motor_st.speed.leftMotor = sp_left[0]
-		self.motor_st.speed.rightMotor = sp_right[0]
-		self.motor_st.voltage = volt[0] 
+		self.motor_st.speed.leftMotor = sp_left
+		self.motor_st.speed.rightMotor = sp_right
+		self.motor_st.voltage = volt
 		sleep(0.01) #Increase time gap between i2c transactions.
 		try:
 			self.measures_pub.publish(motor_st)
