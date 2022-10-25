9a495bddc6b73950ab35237d434fe79c60fc302b
L4rralde <ealarralde@gmail.com>
2022-10-25 16:17:01 -0500

    Adding speed and steering publishers to lane_following node

--- ros_base_nodes/catkin_ws/src/lane_following/src/lane_following_node.cpp
+++ ros_base_nodes/catkin_ws/src/lane_following/src/lane_following_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
+#include <std_msgs/Int16.h>
 #include <sensor_msgs/image_encodings.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -13,16 +14,38 @@ class LaneDetectorNode{
 	image_transport::ImageTransport it_;
 	image_transport::Subscriber image_sub_;
 	image_transport::Publisher image_pub_;
+	ros::Publisher steering_pub;
+	ros::Publisher speed_pub; 
 
 public:
 	LaneDetectorNode() : it_(nh_){
 		image_sub_ = it_.subscribe("/app/camera/rgb/image_raw", 1, &LaneDetectorNode::onImg, this);
 		image_pub_ = it_.advertise("/app/processed/video", 1);
+		steering_pub = nh_.advertise<std_msgs::Int16>("/AutoModelMini/manual_control/steering", 1);
+		speed_pub = nh_.advertise<std_msgs::Int16>("/AutoModelMini/manual_control/speed", 1);
 		cv::namedWindow("Image window");
 	}
 	~LaneDetectorNode(){
 		cv::destroyWindow("Image window");
 	}
+	void publish_steering(float steering){
+		std_msgs::Int16 steering_msg;
+		steering_msg.data = 90*(steering+1);
+		steering_pub.publish(steering_msg);
+	}
+	void publish_speed(float speed){
+		std_msgs::Int16 speed_msg;
+		speed_msg.data = -826.66*speed;
+		speed_pub.publish(speed_msg);
+	}
+	void stop(void){
+		this->publish_speed(0.0);
+	}
+	//Main callback
+	//Receives img from simulation. 
+	//	"cv_ptr->image" is an OpenCV Mat object.
+	//	publish_speed(float speed); updates car's speed in m/s
+	// 	pubilsh_steering(float steering); updates car's steering in degrees (from -90° to 90°)
 	void onImg(const sensor_msgs::ImageConstPtr& msg){
 		cv_bridge::CvImagePtr cv_ptr; 
 		try{
@@ -33,7 +56,12 @@ public:
 		}
 
 		//Frame: cv_ptr->image
-
+		/*
+		if(ros::ok()){
+			this->publish_speed(<speed_value>);
+			this->publish_steering(<steering_value>);
+		}
+		*/
 		cv::imshow("Image window", cv_ptr->image);
 		cv::waitKey(3);
 		image_pub_.publish(cv_ptr->toImageMsg());
