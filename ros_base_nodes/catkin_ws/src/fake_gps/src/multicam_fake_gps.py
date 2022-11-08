cbf3cb4d7a1ffbecb70e1f60632c8f97dfd10625
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2022-11-08 16:51:59 -0600

    Simplifying node and removing time-window sanity check

--- ros_base_nodes/catkin_ws/src/fake_gps/src/multicam_fake_gps.py
+++ ros_base_nodes/catkin_ws/src/fake_gps/src/multicam_fake_gps.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python2
-#Works only for all-same-oriented-fiducial-marks scenarios. To be left this way.
-#Always taking paramters.yaml as camera model, even when gazebo model is the one. To be fixed. Still pending? Not at least in gazebo.
-
-"""
-ros parameters:
-	cam_src = {"internal", "gazebo", "rosbag"}. Tells who is producing the image.
-	upper_cam_id = {0,1,2,...} Tells which upper cam is executing this node.
-	deb_lvl = {0,1,2}. 	0: None
-						1: Publish detected marker's corners.
-						2: Broadcast tf.
-"""
 
 import rospy
 import cv2
-import sys
-import yaml 
 import numpy as np
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
 from camera_misc import CameraModel
 from donkietown_msgs.msg import MarkerEdge, MarkerEdgeArray, Square
 import tf
@@ -47,6 +32,8 @@ class Marker(object):
 		self.frame.broadcast()
 	def set_relative_pose(self,rvec,tvec):
 		self.relH = axis_matrix(rvec,tvec)
+	def get_corners_center(self):
+		return np.mean(self.corners[0], axis=0)
 
 class MobileMarker(Marker):
 	def __init__(self,robot_id,upcam_id,stamp,corners):
@@ -99,35 +86,28 @@ class MobileMarker(Marker):
 		self.posePub.publish(self.pose_msg) #Publish ego mark pose. 
 
 class fake_gps: 
-	def __init__(self, cam=0, arucoDict=cv2.aruco.DICT_4X4_50, cam_id=0,camera_src='internal', refids={'0'}, markerLen=0.1, debug_lvl=0): 
-		self.bridge = CvBridge() #When using rosbag as src image
+	def __init__(self, cam=0, cam_id=0, arucoDict=cv2.aruco.DICT_4X4_50, markerLen=0.1,
+					refids={'0'}, calib_f="calibration.yaml", debug_lvl=0): 
 		self.arucoDict = cv2.aruco.Dictionary_get(arucoDict) 
 		self.markerLen = markerLen #square mark lenght 
 		self.cam_id = cam_id #To allow multiple cameras running
 		self.refids = refids #Set of known reference markers' ids
-		self.ref_marks = {}
 		self.frame_q = queue.Queue(maxsize=1) #To share video frames into different threads 
 		self.time = rospy.Time.now()
 		self.deb_lvl = int(debug_lvl)
 		if self.deb_lvl>1:
 			self.tfBuffer = tf2_ros.Buffer() #This and the following are used for tf transformations
-		#self.listener = tf2_ros.TransformListener(self.tfBuffer) 
+			self.listener = tf2_ros.TransformListener(self.tfBuffer) 
 			#This is recquired to import all TF transformations	
 			#tf broadcast will be only used for debugging purpopses
 		topic_bn = '/sensors/global_camera_'+str(cam_id)
 		self.markerCornersPub = rospy.Publisher(topic_bn+'/marks_corners',MarkerEdgeArray,queue_size=1)
 		
-		calib_f = "calibration_files/cam_"+str(cam_id)+"_calibration_600p.yaml"
 		self.cam_model = CameraModel(paramfile=calib_f,topic=topic_bn+'/info')
-		if camera_src == 'internal': 
-			self.cam = cv2.VideoCapture(cam)
-			self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
-			self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
-		else:
-			self.view_sub = rospy.Subscriber(topic_bn+'/image', Image, self.callback)
-			if camera_src == 'gazebo': 
-				del self.cam_model
-				self.cam_model = CameraModel(kind="subscriber",topic=topic_bn+'/info')
+
+		self.cam = cv2.VideoCapture(cam)
+		self.cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+		self.cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 	def talker(self):
 		proc_thread = threading.Thread(target=self.img_proc_threaded)
 		proc_thread.daemon = True
@@ -143,13 +123,6 @@ class fake_gps:
 		while True:
 			frame = self.frame_q.get()
 			self.get_raw_poses(frame)
-
-	def callback(self,img): #Only executed alongside rosbag.
-		try: 
-			frame = self.bridge.imgmsg_to_cv2(img,'bgr8')
-		except CvBridgeError as e:
-			print(e)
-		self.get_raw_poses(frame)
 
 	def get_raw_poses(self,frame):
 		corners,ids,rejected = cv2.aruco.detectMarkers(frame,self.arucoDict) #Aruco basis function. 
@@ -193,12 +166,9 @@ class fake_gps:
 		if self.deb_lvl>0:
 			self.markerCornersPub.publish(markerArray)
 		#TODO: check current ref_markers sanity.
+		acc_ref_marks = {}
 		for test_rm in ref_marks.values():
 			test_rm_mid = test_rm.mid
-			if not test_rm_mid in self.ref_marks:
-				self.ref_marks[test_rm_mid] = test_rm
-				continue
-			#for good_rm in self.ref_marks.values(): #Avoids updating when camera moves, but might be better
 			for good_rm in ref_marks.values():
 				if test_rm_mid == good_rm.mid:
 					continue
@@ -206,27 +176,18 @@ class fake_gps:
 				(g_r,g_p,g_y) = euler_from_matrix(h_test_to_good[0:3,0:3])
 				(t_xo,t_yo,t_tho) = marks_offset[str(test_rm.mid)]
 				(g_xo,g_yo,g_tho) = marks_offset[str(good_rm.mid)]
-				#print("Results from "+str(test_rm_mid)+" to" +str(good_rm.mid))
-				#print("Relative distances from marks_off:")
-				#print("x: ",g_xo-t_xo)
-				#print("y: ",g_yo-t_yo)
-				#print("th: ",g_tho-t_tho)
-				#print("Relative distances from marker detection")
-				#print("x: ",h_test_to_good[0,3])
-				#print("y: ",h_test_to_good[1,3])
-				#print("th: ",g_y)
+
 				x_dist = g_xo-t_xo-h_test_to_good[0,3]
 				y_dist = g_yo-t_yo-h_test_to_good[1,3]
 				th_dist = g_tho-t_tho-g_y
-				if((x_dist**2+y_dist**2)**0.5 < 0.1):
-					print("Meet sanity criteria")
-					self.ref_marks[test_rm_mid] = test_rm
+				if((x_dist**2+y_dist**2)**0.5 < 0.05):
+					acc_ref_marks[test_rm_mid] = test_rm
 					break
 		for mm in mob_marks.values():
 			#Find nearest reference marker
 			min_dist = 1E9
-			for rm in self.ref_marks.values():
-				dist = np.sum(np.linalg.norm(np.asarray(mm.corners)-np.asarray(rm.corners)))
+			for rm in acc_ref_marks.values():
+				dist = np.linalg.norm(mm.get_corners_center()-rm.get_corners_center())
 				if dist < min_dist: 
 					min_dist = dist
 					near_rm = rm
@@ -239,23 +200,19 @@ class fake_gps:
 				mm.publish()
 			except:
 				print("Couldn't compute absolute pose of mobile marker"+str(mm.mid))
-def main(args):
+def main():
 	rospy.init_node('fakegps',anonymous=True)
-	cam_src = rospy.get_param("~cam_src", "internal")
 	upper_cam_id = rospy.get_param("~upcam_id",0)
+	cam_port = rospy.get_param("~cam_port", 0)
 	deb_lvl = rospy.get_param("~debug_lvl", 0)
+	cam_calib_file = rospy.get_param("~calib_file")
+
 	ref_marks = marks_offset.keys()
-	node = fake_gps(0,cam_id=upper_cam_id,camera_src=cam_src,refids=ref_marks,debug_lvl=deb_lvl) 
-	if not cam_src=='internal':	
-		try:
-			rospy.spin()
-		except KeyboardInterrupt:
-			print("Shutting down")
-	else:
-		try: 
-			node.talker()
-		except rospy.ROSInterruptException:
-			pass
+	node = fake_gps(cam_port,cam_id=upper_cam_id,refids=ref_marks,calib_f=cam_calib_file,debug_lvl=deb_lvl) 
+	try: 
+		node.talker()
+	except rospy.ROSInterruptException:
+		pass
 
 if __name__=='__main__':
-	main(sys.argv)
+	main()
