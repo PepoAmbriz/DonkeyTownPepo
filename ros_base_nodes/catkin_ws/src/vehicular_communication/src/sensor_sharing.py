90eb76538c8c0119f76e1f77bbff501eb3cba87d
L4rralde <larraldeortizemmanuelalejandro@gmail.com>
2022-11-13 20:50:33 -0600

    sensor sharing node is now working with synth data

--- ros_base_nodes/catkin_ws/src/vehicular_communication/src/sensor_sharing.py
+++ ros_base_nodes/catkin_ws/src/vehicular_communication/src/sensor_sharing.py
@@ -1,20 +1,30 @@
 from sklearn.cluster import DBSCAN
-import numpy
+import numpy as np
+import time
 
 class TimedBoundedQueue:
 	def __init__(self,time_window):
 		self.time_window = time_window
-		self.data = np.array([])
+		self.data = []
 		self.time_record = np.array([])
-	def append(self,new_data, dt)
-		np.append(self.data,new_data)
-		self.time_record += dt
-		np.append(self.data,np.zeros(len(new_data)))
+		self.last_time = time.time()
+	def append(self,new_data):
+		self.update()
+		if len(new_data) == 0:
+			return
+		self.time_record = np.append(self.time_record,np.zeros(len(new_data)))
+		for data in new_data:
+			self.data.append(data)
+	def update(self):
+		now = time.time()
+		self.time_record += now-self.last_time
+		self.last_time = now
 	def clean(self):
+		self.update()
 		pop_cnt = 0
-		for t in time_record:
+		for t in self.time_record:
 			if t<self.time_window:
-				return
+				continue
 			pop_cnt += 1
 		self.data = self.data[pop_cnt:]
 		self.time_record = self.time_record[pop_cnt:]
@@ -29,21 +39,31 @@ class SensorSharing:
 	def append(self,data):
 		self.sensorQ.append(data)
 	def cluster(self):
-		self.sensorQ.clean()
-		clustering = DBSCAN(eps=self.cl_eps,min_samples=self.cl_min).fit(self.sensorQ.data)
+		data = self.sensorQ.get_data()
+		if len(data) == 0:
+			return (None,None)
+		clustering = DBSCAN(eps=self.cl_eps,min_samples=self.cl_min).fit(data)
 		clusters_sum = {}
 		clusters_size = {}
-		for i in len(clustering):
-			label = clustering[i]
+		clusters = {}
+		for i in clustering.labels_:
+			label = clustering.labels_[i]
 			if label == -1:
 				continue
-			if not label in clusters_size:
+			if not label in clusters:
+				clusters[label] = [data[i]]
 				clusters_size[label] = 1
-				clusters_sum[label] = self.sensorQ.data[i]
+				clusters_sum[label] = np.array(data[i])
 				continue
+			clusters[label].append(data[i])
 			clusters_size[label] += 1
-			clusters_sum[label] += sensorQ.data[i]
+			clusters_sum[label] += data[i]
+		
 		centers = []
 		for label in clusters_size.keys():
 			centers.append(clusters_sum[label]/clusters_size[label])
-		return centers
+		return (clusters.values(),centers)
+	def get_raw_data(self):
+		return self.sensorQ.get_data()
+	def clean(self):
+		self.sensorQ.clean()
--- ros_base_nodes/catkin_ws/src/vehicular_communication/src/sensor_sharing_node.py
+++ ros_base_nodes/catkin_ws/src/vehicular_communication/src/sensor_sharing_node.py
@@ -1,23 +1,72 @@
-#!/usr/bin/ python3
+#!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import PointCloud
-import sensor_sharing
+from geometry_msgs.msg import Point32
+from sensor_sharing import SensorSharing
 
-class SensorSharingNode:
-	def __init__(self,time_window,rate,q_size):
+def pts2ptmsg(pts):
+	msg_pts = []
+	for pt in pts:
+		ptmsg = Point32()
+		ptmsg.x = pt[0]
+		ptmsg.y = pt[1]
+		msg_pts.append(ptmsg)
+	return msg_pts
+
+class SensorSharingNode(SensorSharing):
+	def __init__(self,time_window,eps,min_samples,rate,q_size,debug):
+		super().__init__(time_window,eps,min_samples)
 		self.time_window = time_window
 		self.rate = rate
-		self.pts_sub = rospy.Subscriber("/v2x/sensors/obstacles/raw", PointCloud, queue_size=q_size)
-		self.obs_pub = rospy.Publisher("/v2x/sensors/obstacles/filtered", PointCloud, self.on_ptcloud, queue_size=1)
+		self.debug_en = debug
+
+		self.pcmsg = PointCloud()
+		self.pcmsg.header.frame_id = "map"
+
+		self.pts_sub = rospy.Subscriber("/v2x/sensors/obstacles/raw", PointCloud, self.on_ptcloud, queue_size=q_size)
+
+		self.obs_pub = rospy.Publisher("/v2x/sensors/obstacles/filtered", PointCloud, queue_size=1)
+		if debug:
+			self.acc_pc_pub = rospy.Publisher("/v2x/sensors/obstacles/pull", PointCloud, queue_size=1)
+
 	def on_ptcloud(self,pcmsg):
+		pts = [[pt.x,pt.y] for pt in pcmsg.points]
+		now = pcmsg.header.stamp.to_sec()
+		self.append(pts)
+	def talker(self):
+		rate = rospy.Rate(self.rate)
+		while not rospy.is_shutdown():
+			self.clean()
+			self.debug()
+			_, filtered_pts = self.cluster()
+			if not filtered_pts:
+				continue
+			self.pcmsg.header.stamp = rospy.Time.now()
+			self.pcmsg.points = pts2ptmsg(filtered_pts)
+			self.obs_pub.publish(self.pcmsg)
+				
+			rate.sleep()
+	def debug(self):
+		if self.debug_en:
+			acc_pc_msg = PointCloud()
+			acc_pc_msg.points = pts2ptmsg(self.get_raw_data())
+			acc_pc_msg.header.frame_id = "map"
+			self.acc_pc_pub.publish(acc_pc_msg)
+
 
 def main():
 	rospy.init_node("sensor_sharing")
-	time_window =  rospy.get_param("~time_window",1.0)
-	rate = rospy.get_param("~rate",10.0)
+	time_window =  rospy.get_param("~time_window",2.0)
+	cluster_eps = rospy.get_param("~cluster_eps",0.1)
+	cluster_min = rospy.get_param("~cluster_min",5)
+	rate = rospy.get_param("~rate",2.0)
 	q_size = rospy.get_param("~queue_size", 5)
-	node = SensorSharingNode(time_window,rate,q_size)
+	debug = rospy.get_param("~debug", True)
+	node = SensorSharingNode(time_window,cluster_eps,cluster_min,rate,q_size,debug)
 	try:
-		node.spin()
-	except ROSInterruptException:
+		node.talker()
+	except rospy.ROSInterruptException:
 		print("Shutting down")
+
+if __name__=='__main__':
+	main()
